302933e787e120a91429699d7875b740470ed904
eduardogarcia2 <eduardogarcia@advantedigital.com>
2024-04-07 04:36:10 -0600

    server

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cors from "cors";
+
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+const client = new MercadoPagoConfig({
+    accessToken: "TEST-7228085904947829-040602-ffad206dcc4ed387ce5f3f6531d4a39a-277588512",
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server iniciado");
+});
+
+app.post("/create_preference", async (req, res) => {
+    try {
+        const preference = new Preference(client);
+        const result = await preference.create({
+            body: {
+                items: req.body.items.map(item => ({
+                    title: item.title,
+                    quantity: item.quantity,
+                    unit_price: item.unit_price,
+                    currency_id: "MXN"
+                })),
+                back_urls: {
+                    success: "http://localhost:5173/success",
+                    failure: "http://localhost:5173/failure",
+                    pending: "http://localhost:5173/pending",
+                },
+                auto_return: "approved",
+            }
+        });
+
+        res.json({
+            id: result.id,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "No jala"
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`El servidor esta corriendo en el puerto ${port}`)
+})
